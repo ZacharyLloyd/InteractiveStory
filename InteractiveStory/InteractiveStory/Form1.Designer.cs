ef7b0617b9485da62275fc1c731b68605ec059d4
Zachary Lloyd <zaclloyd@uat.edu>
2020-06-21 15:59:45 -0400

    Project requirements completed

--- InteractiveStory/InteractiveStory/Form1.Designer.cs
+++ InteractiveStory/InteractiveStory/Form1.Designer.cs
@@ -28,13 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.enterInput = new System.Windows.Forms.Button();
+            this.StoryTextBox = new System.Windows.Forms.TextBox();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.StoryLabel = new System.Windows.Forms.Label();
+            this.InputLabel = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ComboBoxLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enterInput
+            // 
+            this.enterInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterInput.Location = new System.Drawing.Point(317, 128);
+            this.enterInput.Name = "enterInput";
+            this.enterInput.Size = new System.Drawing.Size(75, 35);
+            this.enterInput.TabIndex = 0;
+            this.enterInput.Text = "Enter";
+            this.enterInput.UseVisualStyleBackColor = true;
+            this.enterInput.Click += new System.EventHandler(this.EnterInput_Click);
+            // 
+            // StoryTextBox
+            // 
+            this.StoryTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StoryTextBox.Location = new System.Drawing.Point(41, 380);
+            this.StoryTextBox.Multiline = true;
+            this.StoryTextBox.Name = "StoryTextBox";
+            this.StoryTextBox.ReadOnly = true;
+            this.StoryTextBox.Size = new System.Drawing.Size(673, 297);
+            this.StoryTextBox.TabIndex = 1;
+            this.StoryTextBox.Text = resources.GetString("StoryTextBox.Text");
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(41, 137);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(190, 20);
+            this.InputTextBox.TabIndex = 2;
+            // 
+            // StoryLabel
+            // 
+            this.StoryLabel.AutoSize = true;
+            this.StoryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StoryLabel.Location = new System.Drawing.Point(232, 345);
+            this.StoryLabel.Name = "StoryLabel";
+            this.StoryLabel.Size = new System.Drawing.Size(80, 20);
+            this.StoryLabel.TabIndex = 3;
+            this.StoryLabel.Text = "Story Text";
+            // 
+            // InputLabel
+            // 
+            this.InputLabel.AutoSize = true;
+            this.InputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputLabel.Location = new System.Drawing.Point(87, 104);
+            this.InputLabel.Name = "InputLabel";
+            this.InputLabel.Size = new System.Drawing.Size(84, 20);
+            this.InputLabel.TabIndex = 4;
+            this.InputLabel.Text = "Input Field";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Fight",
+            "Run",
+            "Hide"});
+            this.comboBox1.Location = new System.Drawing.Point(523, 141);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // ComboBoxLabel
+            // 
+            this.ComboBoxLabel.AutoSize = true;
+            this.ComboBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComboBoxLabel.Location = new System.Drawing.Point(519, 104);
+            this.ComboBoxLabel.Name = "ComboBoxLabel";
+            this.ComboBoxLabel.Size = new System.Drawing.Size(148, 20);
+            this.ComboBoxLabel.TabIndex = 6;
+            this.ComboBoxLabel.Text = "Options Drop Down";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = global::InteractiveStory.Properties.Resources.Welcome;
+            this.ClientSize = new System.Drawing.Size(1340, 707);
+            this.Controls.Add(this.ComboBoxLabel);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.InputLabel);
+            this.Controls.Add(this.StoryLabel);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.StoryTextBox);
+            this.Controls.Add(this.enterInput);
+            this.Name = "Form1";
+            this.Text = "InteractiveStory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button enterInput;
+        private System.Windows.Forms.TextBox StoryTextBox;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Label StoryLabel;
+        private System.Windows.Forms.Label InputLabel;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label ComboBoxLabel;
     }
 }
 
--- InteractiveStory/InteractiveStory/Form1.cs
+++ InteractiveStory/InteractiveStory/Form1.cs
@@ -12,9 +12,138 @@ namespace InteractiveStory
 {
     public partial class Form1 : Form
     {
+        //Variable will be used for choices being made from the drop down
+        int newChoice;
+        //Variable to hold players name to display in story
+        string name;
+        //Variable is used to keep track of health and used in losehealth method
+        int health = 100;
         public Form1()
         {
             InitializeComponent();
+        }
+        //Method is used to take in the players name and start the story
+        private void EnterInput_Click(object sender, EventArgs e)
+        {
+            //Get players name
+            name = InputTextBox.Text;
+            //Start story
+            StartStory(name);
+        }
+        //This method is used for the combo box to allow the player to make a decision
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Using the variable we created up top to keep track of what index we are at
+            newChoice = comboBox1.SelectedIndex;
+            //This calls the Decision method
+            Decision();
+        }
+        //This method is used to take away the players health. It returns the health at the end of the method and passes in health as a parameter
+        private int LoseHealth(int loseHealth)
+        {
+            //Set health equal to zero
+            health = 0;
+            //Display to the user that they have no health remaining and may exit the application
+            StoryTextBox.Text = "You have lost all of your health and have now died and turned into a zombie. You may now exit the application";
+            //Return health
+            return health;
+        }
+        //This method starts the story the player will embark on. It returns the name of the player at the end of the method and also passing in the name as a parameter
+        private string StartStory(string playerName)
+        {
+            //Change the background image to SupplyRun which is in the resource folder
+            this.BackgroundImage = Properties.Resources.SupplyRun;
+            //Display the player the story using their name they have entered
+            StoryTextBox.Text = name + " has been choosen to go on a supply run. There is a store not to far from camp that should still have supplies." + name + " will be going with Tom and Suzie. All of you may take whatever you need to survive the supply run.Good luck and God speed. You will have to make a decision once you arrive at the store.";
+            
+            //Commented out to make it all appear at once
+            //StoryTextBox.Text = "There is a store not to far from camp that should still have supplies.";
+            //StoryTextBox.Text = name + "will be going with Tom and Suzie.";
+            //StoryTextBox.Text = "All of you may take whatever you need to survive the supply run.";
+            //StoryTextBox.Text = "Good luck and God speed. You will have to make a decision once you arrive at the store.";
+            
+            //Tell the player they have arrived at the store
+            MessageBox.Show("You have arrived at the store. Use the options drop down to continue");
+            //Change the background image to match to the store located in the resource folder
+            this.BackgroundImage = Properties.Resources.Store;
+            //Return the name of the player
+            return name;
+        }
+        //This method is used for the decision the player will make
+        //Depending on what option they picked in the drop down will depend on what outcome they get
+        private void Decision()
+        {
+            //If the player picks the first item in the drop down
+            if(newChoice == 0)
+            {
+                //Show the first case in continuestory
+                ContinueStory(1);
+            }
+            //If the player picks the second item in the drop down
+            else if(newChoice == 1)
+            {
+                //Show the second case in continuestory
+                ContinueStory(2);
+            }
+            //If the player picks the third item in the drop down
+            else if(newChoice == 2)
+            {
+                //Show the third case in continuestory
+                ContinueStory(3);
+            }
+        }
+        private void ContinueStory(int index)
+        {
+            //Change background image to match zombiefight found in resource folder
+            this.BackgroundImage = Properties.Resources.ZombieFight;
+            //Tell the user they have looted everything and zombies are spotted
+            StoryTextBox.Text = "You guys have gotten to the store and looted everything. Tom notices a horde of zombies approaching from the street.";
+            //Switch for the different outcomes related to what decision the player has made
+            switch (index)
+            {
+                //If the player has picked the first item in the drop down this is their story
+                case 1:
+                    //Display story
+                    StoryTextBox.Text = "You have decided to fight the horde. You all take apportiate positions to fend off the horde. You are able to fend them off for a short time but all of you run out of bullets. There is nothing left for you guys to do except to accept fate.";
+                    
+                    //Commented out to make it all appear at once
+                    //StoryTextBox.Text = "You all take apportiate positions to fend off the horde.";
+                    //StoryTextBox.Text = "You are able to fend them off for a short time but all of you run out of bullets.";
+                    //StoryTextBox.Text = "There is nothing left for you guys to do except to accept fate.";
+                    
+                    //Thank the player for playing and that they can now exit
+                    MessageBox.Show("Thank you for playing the simulation is now over you may exit the application now");
+                    break;
+                //If the player has picked the second item in the drop down this is their story
+                case 2:
+                    //Display Story
+                    StoryTextBox.Text = "You have decided to run from the horde. You make it down the street and think all of you are getting away. You turn a street corner and realize you are surrounded though and have no where to go. You try fighting at this point but are overwhelmed.";
+
+                    //Commented out to make it all appear at once
+                    //StoryTextBox.Text = "You make it down the street and think all of you are getting away.";
+                    //StoryTextBox.Text = "You turn a street corner and realize you are surrounded though and have no where to go.";
+                    //StoryTextBox.Text = "You try fighting at this point but are overwhelmed.";
+
+                    //Thank the player for playing and that they can now exit
+                    MessageBox.Show("Thank you for playing the simulation is now over you may exit the application now");
+                    break;
+                //If the player has picked the third item in the drop down this is their story
+                case 3:
+                    //Display story
+                    StoryTextBox.Text = "You have decided to hide from the horde. All of you find a good spot in a warehouse to hide for the time being. Something in the warehouse makes a loud crashing noise though and alerts the horde. You are now trapped in a warehouse with the horde closing in. You all try to fight for your lives but it is not enough.";
+
+                    //Commented out to make it all appear at once
+                    //StoryTextBox.Text = "All of you find a good spot in a warehouse to hide for the time being.";
+                    //StoryTextBox.Text = "Something in the warehouse makes a loud crashing noise though and alerts the horde.";
+                    //StoryTextBox.Text = "You are now trapped in a warehouse with the horde closing in.";
+                    //StoryTextBox.Text = "You all try to fight for your lives but it is not enough.";
+
+                    //Thank the player for playing and that they can now exit
+                    MessageBox.Show("Thank you for playing the simulation is now over you may exit the application now");
+                    break;
+            }
+            //Call the LoseHealth method to let the player know they have died
+            LoseHealth(health);
         }
     }
 }
